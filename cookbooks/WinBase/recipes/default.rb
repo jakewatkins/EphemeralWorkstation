b9cd2dae2f5cea4397e65f963ed86a81ca129293
jake <jake.watkins@gmail.com>
2022-06-28 10:41:33 -0500

    working!

--- cookbooks/WinBase/recipes/default.rb
+++ cookbooks/WinBase/recipes/default.rb
@@ -11,12 +11,15 @@
 #     EOH
 # end
 
-install_sql = ::File.exist?('sql.txt')
-install_vs = ::File.exist?('visualstudio.txt')
+install_sql = ::File.exist?('c:/sql.txt')
+install_vscode = ::File.exist?('c:/vscode.txt')
+install_vs = ::File.exist?('c:/visualstudio.txt')
+install_chrome = ::File.exist?('c:/chrome.txt')
 
 puts ("sql installed: " + install_sql.to_s )
+puts (" vs code installed: " + install_vscode.to_s)
 puts ("vs installed: " + install_vs.to_s )
-
+puts ("chrome installed: " + install_chrome.to_s)
 
 chef_client_scheduled_task 'Run Chef Infra Client as a scheduled task'
 
@@ -54,31 +57,41 @@ windows_package 'dotnet-sdk-6.0.301-win-x64.exe' do
 end
 
 #install Chrome
-windows_package 'ChromeSetup.exe' do
-    action :install
-    installer_type :custom
-    options '/silent /install'
-    source 'https://www.google.com/chrome/thank-you.html?brand=CHBF&statcb=1&installdataindex=empty&defaultbrowser=0#'
+if !install_chrome
+    windows_package 'ChromeSetup.exe' do
+        action :install
+        installer_type :custom
+        options '/silent /install'
+        source 'https://jkwfiles.blob.core.windows.net/bootstrap/ChromeSetup.exe?sp=r&st=2022-06-27T13:28:37Z&se=2024-01-01T22:28:37Z&spr=https&sv=2021-06-08&sr=b&sig=D2Fc1Oy9s8K2xIQ2fLp1YWqT%2BPfPAPoRkFfVHMmDVuk%3D'
+    end
+
+    # powershell_script 'wait-for-setup' do
+    #     code <<-EOH
+    #     wait-process -name "ChromeSetup" -ErrorAction SilentlyContinue
+    #     EOH
+    # end
+
+    file 'c:/chrome.txt' do
+        content "installed"
+    end
 end
 
 #Visual Studio Code
-windows_package 'VSCodeUserSetup-x64-1.68.1.exe' do
-    action :install
-    installer_type :custom
-    options '/VERYSILENT /NORESTART /MERGETASKS=!runcode'
-    source 'https://jkwfiles.blob.core.windows.net/bootstrap/VSCodeSetup-x64-1.68.1.exe?sp=r&st=2022-06-26T17:27:51Z&se=2022-06-27T01:27:51Z&spr=https&sv=2021-06-08&sr=b&sig=1BFtXuJTVoOmvbl6i%2F0GSLm5PuJlId9ydFa28WT9WiU%3D'
-end
-
-if !install_vs
-    #Visual Studio Pro
-    windows_package 'VisualStudioSetup' do
+if !install_vscode
+    windows_package 'VSCodeUserSetup-x64-1.68.1.exe' do
         action :install
         installer_type :custom
-        options '--add Microsoft.VisualStudio.Product.Professional --IncludeRecommended --quiet --norestart'
-        source 'https://jkwfiles.blob.core.windows.net/bootstrap/VisualStudioSetup.exe?sp=r&st=2022-06-26T17:27:03Z&se=2022-06-27T01:27:03Z&spr=https&sv=2021-06-08&sr=b&sig=8UNRzzPFp1TURw0kdPFo%2BmcRnC%2BC7Ur%2B5hKxT%2BmyZ0c%3D'
+        options '/VERYSILENT /NORESTART /MERGETASKS=!runcode'
+        source 'https://jkwfiles.blob.core.windows.net/bootstrap/VSCodeSetup-x64-1.68.1.exe?sp=r&st=2022-06-27T13:26:31Z&se=2024-01-01T22:26:31Z&spr=https&sv=2021-06-08&sr=b&sig=6LFpoKf38i7gUOmZBdtwOQTTvguHufAc1V6yTTffzLg%3D'
     end
 
-    file 'c:/visualstudio.txt' do
+    # powershell_script 'wait-for-setup' do
+    #     code <<-EOH
+    #     wait-process -name "VSCodeSetup-x64-1.68.1" -ErrorAction SilentlyContinue
+    #     EOH
+    # end
+
+    file 'c:/vscode.txt' do
         content "installed"
     end
 end
@@ -97,10 +110,39 @@ if !install_sql
         source 'C:/packages/SSMS-Setup-ENU.exe'
     end
 
+    #wait until setup exits before continueing
+    # powershell_script 'wait-for-setup' do
+    #     code <<-EOH
+    #     wait-process -name "setup" -ErrorAction SilentlyContinue
+    #     EOH
+    # end
+    
     file 'c:/sql.txt' do
         content "installed"
     end
 end
+
+if !install_vs
+    #Visual Studio Pro
+    windows_package 'VisualStudioSetup' do
+        action :install
+        installer_type :custom
+        options '--add Microsoft.VisualStudio.Product.Professional --add Microsoft.VisualStudio.Component.Web --add Microsoft.VisualStudio.ComponentGroup.Web --add Microsoft.VisualStudio.ComponentGroup.WebToolsExtensions  --includeRecommended --includeOptional --quiet --norestart'
+        source 'https://jkwfiles.blob.core.windows.net/bootstrap/VisualStudioSetup.exe?sp=r&st=2022-06-27T13:27:40Z&se=2024-01-01T22:27:40Z&spr=https&sv=2021-06-08&sr=b&sig=6q%2FPrBA6zluywNm5lHBZsEwCMDsJ%2FTRcB0Z6GprWoAw%3D'
+    end
+
+    #wait until setup exits before continueing
+    # powershell_script 'wait-for-setup' do
+    #     code <<-EOH
+    #     wait-process -name "setup" -ErrorAction SilentlyContinue
+    #     EOH
+    # end
+    
+    file 'c:/visualstudio.txt' do
+        content "installed"
+    end
+end
+
 
 file 'c:/win-dev-workstation.txt' do
     content node.default['windows_base']['cookbook_version']
